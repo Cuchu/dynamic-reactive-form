f00be4508eae4896e37b6e021adbe4599ae22168
Maximiliano <schvindt@gmail.com>
2018-08-05 20:56:54 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+/* https://angular-templates.io/tutorials/about/angular-forms-and-validations */
+/* https://code.tutsplus.com/es/tutorials/introduction-to-forms-in-angular-4-reactive-forms--cms-29787 */
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { dynamic } from './dynamic';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'dynamic-form';
+  dataObject = dynamic;
+  form: FormGroup;
+  objectProps:Array<any>;
+  
+  constructor() {}
+
+  ngOnInit() {
+    console.log("ngOnInit");
+    // remap the API to be suitable for iterating over it
+    this.objectProps = Object.keys(this.dataObject).map(prop => {
+        return Object.assign({}, { key: prop} , this.dataObject[prop]);
+    });
+
+    console.log(this.objectProps);
+
+    // setup the form
+    const formGroup = {};
+
+    for(let prop of Object.keys(this.dataObject)) {
+      if(this.dataObject[prop].group) {
+        
+        var subGroup = {};
+        for(let subProp of Object.keys(this.dataObject[prop].elements)) {
+          var element = this.dataObject[prop].elements[subProp];
+          subGroup[element.key] = new FormControl(element.value || '', this.mapValidators(element.validation));
+        }
+        formGroup[prop] = new FormGroup(subGroup);
+      } else {
+        formGroup[prop] = new FormControl(this.dataObject[prop].value || '', this.mapValidators(this.dataObject[prop].validation));
+      }
+    }
+    
+    console.log(formGroup);
+
+    this.form = new FormGroup(formGroup);
+  }
+
+  private mapValidators(validators) {
+    const formValidators = [];
+    if(validators) {
+      for(const validation of Object.keys(validators)) {
+        if(validation === 'required') {
+          formValidators.push(Validators.required);
+        } else if(validation === 'email') {
+          formValidators.push(Validators.email);
+        } else if(validation === 'min') {
+          formValidators.push(Validators.min(validators[validation]));
+        } else if(validation === 'max') {
+          formValidators.push(Validators.max(validators[validation]));
+        } else if(validation === 'minLength') {
+          formValidators.push(Validators.minLength(validators[validation]));
+        } else if(validation === 'maxLength') {
+          formValidators.push(Validators.maxLength(validators[validation]));
+        }
+      }
+    }
+
+    console.log(formValidators);
+    return formValidators;
+  }
+
+  onSubmit(form) {
+    console.log(form);
+  }
 }
---
+++ src/app/dynamic.ts
@@ -0,0 +1,87 @@
+export const dynamic = {
+    name: {
+        label: 'Name',
+        value: null,
+        type: 'text',
+        validation: {
+            required: true,
+            minLength: 5
+        }
+    },
+    surname: {
+        label: 'Surname',
+        value: null,
+        type: 'text',
+        validation: {
+            required: true
+        }
+    },
+    email: {
+        label: 'Email',
+        value: null,
+        type: 'email',
+        validation: {
+            required: true,
+            email: true
+        }
+    },
+    age: {
+        label: 'Age',
+        value: 32,
+        type: 'number',
+        validation: {
+            min: 18,
+            max: 50,
+            required: true
+        }
+    },
+    gender: {
+        label: 'Gender',
+        value: 'M',
+        type: 'radio',
+        options: [
+            { label: "Male", value: 'M'},
+            { label: "Female", value: 'F'},
+            { label: "Indefined", value: 'I'}
+        ]
+    }, 
+    city: {
+        label: 'City',
+        value: '2000',
+        type: 'select',
+        options: [
+            { label: "(choose one)", value: ''},
+            { label: "Rosario", value: '2000'},
+            { label: "Nogoyá", value: '3150'},
+            { label: "Santa Fe", value: '3000'}
+        ],
+        validation: {
+            required: true
+        }
+    },
+    extra: {
+        key: 'extra',
+        group: true,
+        elements: [
+            {
+                key: 'hobby',
+                label: 'Hobby',
+                value: null,
+                type: 'text',
+                validation: {
+                    required: true,
+                    minLength: 5
+                }    
+            },{
+                key: 'description',
+                label: 'Description',
+                value: null,
+                type: 'text',
+                validation: {
+                    required: true,
+                    minLength: 5
+                }    
+            }
+        ]
+    }
+  }
