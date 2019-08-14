c8b26b167d0bf5cf23339bbebd87b20cd97a6690
Maximiliano Schvindt <schvindt@gmail.com>
2019-08-14 15:50:10 -0300

    Added css

--- src/app/object-html.ts
+++ src/app/object-html.ts
@@ -1,42 +1,42 @@
 export const object = {
-    header: {
-        col1: '<b>Post Hospital Order Date:</b> 07/02/2019 15:30 (599813436)<br />Walton, Robert (23456)<br /><b>Episode:</b> 07/08/19 - 09/05/19<br /><b>Resumption of Care Date:</b> 07/10/2019',
-        col2: '<b>West Meadows Home Health</b><br />121314 Downing Lane<br />Austin, Texas 78651<br />(512) 867 - 5309'
+    "header": {
+        "col1": "<b>Post Hospital Order Date:</b> 07/02/2019 15:30 (599813436)<br />Gonzalo, Tieeeer (xxxxxx)<br /><b>Episode:</b> 07/08/19 - 09/05/19<br /><b>Resumption of Care Date:</b> 07/10/2019",
+        "col2": "<b>XXX YYY Home Health</b><br />121314 Address Lane<br />Austin, Vegas 78651<br />(512) 867 - 00000"
     },
-    body: [
+    "body": [
         {
-            title: 'Inpatient Information',
-            content: [
-                {line: '<b>Hospital Stay From:</b> 06/26/2019 to 6/29/2019'},
-                {line_header: 'Surgical Procedures'},
-                {col1:'F14Z34Z Binaural Hear Aid Assess w Acoust Immit/Reflex Equip',col2:'<b>Date:</b> 06/25/2019'},
-                {col1:'Z71 Removal of organs to be rinsed and repainted',col2:'<b>Date:</b> 06/25/2019'},
-                {line_header: 'Inpatient Diagnosis'},
-                {line: 'F14Z45 Encounter for supervision of normal radiation synthesis with arachnids<br />Z220 Carrier of typhoid'},
-                {line_header: 'Primary Diagnosis'},
-                {col1:'I13 Hypertensive heart and chronic kidney disease',col2:'<b>Date:</b> 06/25/2019'},
-                {line_header: 'Secondary Diagnosis'},
-                {col1:'I13 Hypertensive heart and chronic kidney disease',col2:'<b>Date:</b> 06/25/2019'}
+            "title": "Inpatient Information",
+            "content": [
+                {"line": "<b>Hospital Stay From:</b> 06/26/2019 to 6/29/2019"},
+                {"line_header": "Surgical Procedures"},
+                {"col1":"F14Z34Z Binaural Hear Aid Assess w Acoust Immit/Reflex Equip","col2":"<b>Date:</b> 06/25/2019"},
+                {"col1":"Z71 Removal of organs to be rinsed and repainted","col2":"<b>Date:</b> 06/25/2019"},
+                {"line_header": "Inpatient Diagnosis"},
+                {"line": "F14Z45 Encounter for supervision of normal radiation synthesis with arachnids<br />Z220 Carrier of typhoid"},
+                {"line_header": "Primary Diagnosis"},
+                {"col1":"I13 Hypertensive heart and chronic kidney disease","col2":"<b>Date:</b> 06/25/2019"},
+                {"line_header": "Secondary Diagnosis"},
+                {"col1":"I13 Hypertensive heart and chronic kidney disease","col2":"<b>Date:</b> 06/25/2019"}
             ]
         },{
-            title: 'Risk Profile',
-            content: [
-                {line: 'Risk Factors: Currently taking 5 or more medications. Currently reports exhaustion. Decline in mental, emotional or behavioral status in past 3 months. <br />Additional Risk Information: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'}
+            "title": "Risk Profile",
+            "content": [
+                {"line": "Risk Factors: Currently taking 5 or more medications. Currently reports exhaustion. Decline in mental, emotional or behavioral status in past 3 months. <br />Additional Risk Information: At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga."}
             ]
         },{
-            title: 'Medications',
-            content: [
-                {line: 'Fexofenadine HCl Oral 180 MG 2 Tab(s) feqrqewrr (N)<br />Fexofenadine HCl Oral 60 MG 1 Tab(s) 453635463456 rwtgerert (N)<br />Teduglutide (rDNA) Subcutaneous 5 MG 1 errewr'}
+            "title": "Medications",
+            "content": [
+                {"line": "Fexofenadine HCl Oral 180 MG 2 Tab(s) feqrqewrr (N)<br />Fexofenadine HCl Oral 60 MG 1 Tab(s) 453635463456 rwtgerert (N)<br />Teduglutide (rDNA) Subcutaneous 5 MG 1 errewr"}
             ]
         },{
-            title: 'Orders and Treatments',
-            content: [
-                {line: 'Advance Directives? Yes.<br />Intent: DNR, Living Will, Medical Power of Attorney (Joe Roberts, (315) 787-7878)<br />Copies on file with Agency? Yes.<br />Surrogate: No<br />Patient was provided written and verbal information on Advance Directives? Yes.<br />SN: Effective 8/12/19 5w3, .<br />PT: Effective 3/14/19 3w2, .<br />Additional orders information typed into here.<br />Assessment of patient with.<br />Is the Patient DNR (Do Not Resuscitate)? Yes.<br />Homebound Status: Confusion, unsafe to go out of home alone,Requires max assistance/taxing effort to leave home,Residual weakness,Unable to safely leave home unassisted,Severe SOB or SOB upon exertion.<br />Notify physician of: Temperature greater than 45 or less than 361.<br />Pulse greater than 12 or less than 12.<br />Respirations greater than 12 or less than 45.<br />Systolic BP greater than 78 or less than 45.<br />Diastolic BP greater than 12 or less than 45.<br />O2 Sat less than 45%.<br />Fasting blood sugar greater than 45 or less than 12.<br />Random blood sugar greater than 45 or less than 12.<br />Weight greater than 45 lbs or less than 45 lbs.'}
+            "title": "Orders and Treatments",
+            "content": [
+                {"line": "Advance Directives? Yes.<br />Intent: DNR, Living Will, Medical Power of Attorney (Joe Roberts, (315) 787-7878)<br />Copies on file with Agency? Yes.<br />Surrogate: No<br />Patient was provided written and verbal information on Advance Directives? Yes.<br />SN: Effective 8/12/19 5w3, .<br />PT: Effective 3/14/19 3w2, .<br />Additional orders information typed into here.<br />Assessment of patient with.<br />Is the Patient DNR (Do Not Resuscitate)? Yes.<br />Homebound Status: Confusion, unsafe to go out of home alone,Requires max assistance/taxing effort to leave home,Residual weakness,Unable to safely leave home unassisted,Severe SOB or SOB upon exertion.<br />Notify physician of: Temperature greater than 45 or less than 361.<br />Pulse greater than 12 or less than 12.<br />Respirations greater than 12 or less than 45.<br />Systolic BP greater than 78 or less than 45.<br />Diastolic BP greater than 12 or less than 45.<br />O2 Sat less than 45%.<br />Fasting blood sugar greater than 45 or less than 12.<br />Random blood sugar greater than 45 or less than 12.<br />Weight greater than 45 lbs or less than 45 lbs."}
             ]
         }
     ],
-    footer: {
-        col1: '<b>West Meadows Home Health</b>',
-        col2: '121314 Downing Lane<br />Austin, Texas 78651',
+    "footer": {
+        "col1": "<b>XXX YYY Home Health</b>",
+        "col2": "121314 Address Lane<br />Austin, Vegas 78651"
     }
-  }
+}
